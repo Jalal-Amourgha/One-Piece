44c58b26cf0f775a26df86c6ac000ecf9d04c344
Jalal-Amourgha <myproo100@gmail.com>
2023-10-24 13:55:12 +0100

    Adding The Review Section

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let list = document.querySelectorAll("ul li a");
 const li = document.querySelectorAll("ul li a");
 const sections = document.querySelectorAll("section");
 
+// This Function Allow Us To Switch The Color of Link Element Depend on The Section We Are In
 function activeMenu() {
   let len = sections.length;
   while (--len && window.scrollY + 97 < sections[len].offsetTop) {}
@@ -13,6 +14,7 @@ activeMenu();
 
 window.addEventListener("scroll", activeMenu);
 
+// Changing The Color Of Link Element When clicked
 list.forEach((e) => {
   e.addEventListener("click", removeActive);
 });
@@ -23,3 +25,34 @@ function removeActive() {
     this.classList.add("active");
   });
 }
+
+// Zooming When User Hover On Map Image
+var zoomContainer = document.querySelector(".zoom-container");
+var zoomImage = document.querySelector("#zoom-image");
+
+zoomContainer.addEventListener("mousemove", function (e) {
+  var containerWidth = zoomContainer.offsetWidth;
+  var containerHeight = zoomContainer.offsetHeight;
+
+  var mouseX = e.pageX - zoomContainer.offsetLeft;
+  var mouseY = e.pageY - zoomContainer.offsetTop;
+
+  var percentX = mouseX / containerWidth;
+  var percentY = mouseY / containerHeight;
+
+  var zoomedWidth = zoomImage.offsetWidth * 5;
+  var zoomedHeight = zoomImage.offsetHeight * 5;
+
+  var offsetX = (zoomedWidth - containerWidth) / 25;
+  var offsetY = (zoomedHeight - containerHeight) / 25;
+
+  var moveX = percentX * offsetX * -2;
+  var moveY = percentY * offsetY * -2;
+
+  zoomImage.style.transform =
+    "scale(1.5) translate(" + moveX + "px, " + moveY + "px)";
+});
+
+zoomContainer.addEventListener("mouseleave", function () {
+  zoomImage.style.transform = "scale(1) translate(0, 0)";
+});
